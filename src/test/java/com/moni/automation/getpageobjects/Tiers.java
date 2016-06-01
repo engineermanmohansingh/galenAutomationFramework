10414e30b72b1077e0f72c591fd693c5893734f8
Manmohan Singh <manmohansingh@qainfotech.net>
2016-06-01 12:26:27 +0530

    Added advanced selenium utilities .

---
+++ src/test/java/com/moni/automation/getpageobjects/Tiers.java
@@ -0,0 +1,12 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.moni.automation.getpageobjects;
+
+
+public enum Tiers {
+    PROD, prod, production, PRODUCTION, Production,
+    QA, Qa, qa;
+   ;
+}
